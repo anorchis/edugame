b2b264601251fd3ce577a74fb6527858e72884a1
anorchis2 <anorchis2@gmail.com>
2026-02-01 09:25:17 +0000

    mobile

--- mobile-detect.js
+++ mobile-detect.js
@@ -10,40 +10,18 @@
         return;
     }
 
-    // 웹 버전 강제 접속 플래그 확인
-    if (sessionStorage.getItem('forceDesktop') === 'true') {
-        return;
-    }
-
     // 모바일 감지 함수
     function isMobileDevice() {
-        // User Agent 기반 감지
         const userAgent = navigator.userAgent || navigator.vendor || window.opera;
         const mobileRegex = /Android|webOS|iPhone|iPad|iPod|BlackBerry|IEMobile|Opera Mini/i;
-
-        // 터치 지원 + 화면 크기 기반 감지 (태블릿 포함)
         const hasTouchScreen = 'ontouchstart' in window || navigator.maxTouchPoints > 0;
         const isSmallScreen = window.innerWidth <= 1024;
-
         return mobileRegex.test(userAgent) || (hasTouchScreen && isSmallScreen);
     }
 
     // 모바일 페이지로 리다이렉트
     function redirectToMobile() {
-        const currentPath = window.location.pathname;
-        const currentFile = currentPath.split('/').pop() || 'index.html';
-
-        // 현재 페이지에 맞는 모바일 페이지로 이동
-        let mobilePath = '/mobile/';
-        if (currentFile === 'stage1.html') {
-            mobilePath += 'stage1.html';
-        } else if (currentFile === 'stage2.html') {
-            mobilePath += 'stage2.html';
-        } else {
-            mobilePath += 'index.html';
-        }
-
-        window.location.href = mobilePath;
+        window.location.href = 'mobile/index.html';
     }
 
     // 모바일 기기면 자동 리다이렉트
